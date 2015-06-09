9c8dcb4e205b3911d7c74b1b69bdfce883e2e152
AngeLopez <angel@angelopez.ca>
2015-06-09 16:44:48 -0400

    Banana alert added

--- exercise6.rb
+++ exercise6.rb
@@ -1,10 +1,14 @@
 # Exercise 6 for Ruby
 #Grocery shopping!
 
-grocery_list = ["chicken","almonds","spinach","eggs","tea"]
+grocery_list = ["chicken","almonds","spinach","eggs","tea", "bananas"]
 
 grocery_list.each do |item|
 	puts "* " + item
 end
 
 puts "There are #{grocery_list.count} items in your Grocery List"
+
+puts grocery_list.include?("bananas")? "You need to pick up bananas.": "You don't need to pick up bananas today." 
+
+puts grocery_list[1]
