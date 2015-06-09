9594ad5b1528a4c0a6efb40f5ee2e5eda83735f5
AngeLopez <angel@angelopez.ca>
2015-06-09 16:19:51 -0400

    Method to convert temps made

---
+++ exercise5.rb
@@ -0,0 +1,16 @@
+# Convert Fahrenheit to Celcius
+
+puts "What is the temperature in Fahrenheit?"
+f_temp = gets.chomp.to_i
+
+#get input for temperature to convert
+
+def TempConvert(input)
+	((input - 32) * (5.to_f/9.to_f)).to_i
+end
+# integers do not return decimals. Floats are decimals that can be multiplied
+# revert return back to integer to remove decimals 
+
+c_temp = TempConvert(f_temp)
+
+puts "#{f_temp} degrees Fahrenheit is equivalent to #{c_temp} degrees Celcius"
