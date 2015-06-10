aadf91ae39698250216bf4a24d500459169c17a2
AngeLopez <angel@angelopez.ca>
2015-06-10 15:08:17 -0400

    Problems 1-4 completed and commented

--- exercise7.rb
+++ exercise7.rb
@@ -1,18 +1,28 @@
 students = {
 	:cohort1 => 34,
-	:cohort2 => 42,
+  :cohort2 => 42,
 	:cohort3 => 22,
-	:cohort4 => 43,
 }
 
-def display(name, num)
-	puts "#{name}: #{num} students"
+def display(name)
+  puts "Bitmaker Cohort Sizes:"
+  name.each do |name, num|
+  puts "#{name}: #{num} students"
+end
 end
 
+# Display name and number
 display(students)
 
+# Add 43 as number of students in Cohort 4
+students[:cohort4] = 43
+display(students)
+
+# Output cohort names with .keys
 puts students.keys
 
+# Classroom growth
+puts "Bitmaker Cohort Size Increse:"
 students.each do |name, num|
   increase = (num * 1.05).to_i
   puts "#{name}: "+ increase.to_s + " students"
