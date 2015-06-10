0747d2126ca73edb6ed3ffa30e6d26b9cccb1594
AngeLopez <angel@angelopez.ca>
2015-06-10 14:32:08 -0400

    Problems 1-5 commented

--- exercise6.rb
+++ exercise6.rb
@@ -3,10 +3,11 @@
 
 grocery_list = ["carrots", "toilet paper", "apples", "salmon", "rice", "bananas"]
 
+# Your next step should present your grocery list with an item on each line, with an asterisk (*) in front of it
 grocery_list.each do |item|
 	puts "* " + item
 end
-
+# Write a method
 def lister(list)
   list.each do |item|
     puts "* " + item.to_s
@@ -15,8 +16,11 @@ end
 
 lister(grocery_list)
 
+# Output the total number of items on your list.
 puts "There are #{grocery_list.count} items in your Grocery List"
 
+# Check to see if your list includes "bananas"
 puts grocery_list.include?("bananas")? "You need to pick up bananas.": "You don't need to pick up bananas today."
 
+# Display the second item in the list
 puts "2nd item on your list is: #{grocery_list[1]}"
