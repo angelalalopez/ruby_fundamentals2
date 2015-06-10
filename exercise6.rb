9daede143fce5d98645184b7ddd5d243b5a8db99
AngeLopez <angel@angelopez.ca>
2015-06-10 09:38:22 -0400

    Method for displaying hash created

--- exercise6.rb
+++ exercise6.rb
@@ -1,14 +1,20 @@
 # Exercise 6 for Ruby
 #Grocery shopping!
 
-grocery_list = ["chicken","almonds","spinach","eggs","tea", "bananas"]
+grocery_list = ["chicken","almonds","spinach","eggs","salmon", "bananas"]
 
 grocery_list.each do |item|
 	puts "* " + item
+end
+
+def lister(list) 
+	puts "* " + list.to_s
 end
 
 puts "There are #{grocery_list.count} items in your Grocery List"
 
 puts grocery_list.include?("bananas")? "You need to pick up bananas.": "You don't need to pick up bananas today." 
 
-puts grocery_list[1]
+puts "2nd item on your list is: #{grocery_list[1]}"
+
+puts lister(grocery_list)
--- exercise7.rb
+++ exercise7.rb
@@ -2,11 +2,8 @@ students = {
 	:cohort1 => 34,
 	:cohort2 => 42,
 	:cohort3 => 22,
-	:cohort4 => 43,
 }
 
 students.each do |name, num|
 	puts "#{name}: #{num} students"
 end
-
-puts students.keys 
